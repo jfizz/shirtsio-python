3573a31120b83c2f473c838249774c59290fd9fc
Dean Song <dean.song@movit-tech.com>
2013-05-08 15:21:08 +0800

    Update example.py

--- example.py
+++ example.py
@@ -1,10 +1,11 @@
 import shirtsio
 
-shirtsio.resources.api_key='a086134c5625ebfd4e080d19749bc0cb736ad1d5'
-
 # get api key
-auth_resp = shirtsio.Account.integration_auth({'username': 'deantest', 'password': 'Pa$$w0rd'})
+auth_resp = shirtsio.Account.integration_auth({'username': 'yourtestuser', 'password': 'Pa$$w0rd'})
 print auth_resp['api_key']
+
+
+shirtsio.resources.api_key=auth_resp['api_key']
 
 # get quote
 quote_resp = shirtsio.Quote.get_quote({'garment[0][product_id]': 3, 'garment[0][color]': 'White', 'garment[0][sizes][med]': 100,
